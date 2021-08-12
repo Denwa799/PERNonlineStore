2ca015fa5d1d89ba02d27b634925bb2ac512acc7
Denwa <denwa799@yandex.ru>
2021-08-12 14:51:41 +0300

    Add tables in db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-
+const models = require('./models/models')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'WORKING!!!'})
+})
 
 const start = async () => {
     try {
@@ -15,3 +22,5 @@ const start = async () => {
         console.log(e)
     }
 }
+
+start()
